a7a848853a1b090c9174d883507bcb0c25f22d59
Kamacio <info@mauroaluffi.com>
2026-05-27 22:19:29 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         messages: [
           {
             role: 'system',
-            content: knowledgeBase`
+            content: knowledgeBase
 `
           },
           {
